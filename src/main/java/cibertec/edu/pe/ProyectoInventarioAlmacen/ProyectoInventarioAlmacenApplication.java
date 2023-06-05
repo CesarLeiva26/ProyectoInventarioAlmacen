9e0f8e9af816caecc251bd00f075a71e51039c38
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-06-05 18:26:51 -0500

    initial commit

---
+++ src/main/java/cibertec/edu/pe/ProyectoInventarioAlmacen/ProyectoInventarioAlmacenApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.ProyectoInventarioAlmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInventarioAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInventarioAlmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/ProyectoInventarioAlmacen/ProyectoInventarioAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.ProyectoInventarioAlmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInventarioAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
